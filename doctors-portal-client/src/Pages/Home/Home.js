85a803c7252f5ced06961b8ebe1c77f5c090c28f
crescentpartha <crescentpartha406@gmail.com>
2023-01-02 00:58:10 +0600

    Responsive Grid for services section

--- doctors-portal-client/src/Pages/Home/Home.js
+++ doctors-portal-client/src/Pages/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Banner from './Banner';
+import Dental from './Dental';
 import Info from './Info';
+import Services from './Services';
 
 const Home = () => {
     return (
-        <div>
+        <div className='px-12'>
             <Banner></Banner>
             <Info></Info>
+            <Services></Services>
+            <Dental></Dental>
         </div>
     );
 };
--- doctors-portal-client/src/Pages/Home/Info.js
+++ doctors-portal-client/src/Pages/Home/Info.js
@@ -6,7 +6,7 @@ import phone from '../../assets/icons/phone.svg';
 
 const Info = () => {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-12'>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
             <InfoCard cardTitle="Opening Hours" bgClass="bg-gradient-to-r from-cyan-500 to-blue-500" img={clock}></InfoCard>
             <InfoCard cardTitle="Our Locations" bgClass="bg-accent" img={marker}></InfoCard>
             <InfoCard cardTitle="Contact Us" bgClass="bg-gradient-to-r from-cyan-500 to-blue-500" img={phone}></InfoCard>
