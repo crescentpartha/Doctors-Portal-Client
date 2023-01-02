0492bf7d421bbbd762e72d3cceafbe95df35d55d
crescentpartha <crescentpartha406@gmail.com>
2023-01-02 23:45:59 +0600

    Create Appointment Page

--- doctors-portal-client/src/Pages/Home/Home.js
+++ doctors-portal-client/src/Pages/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Banner from './Banner';
 import Dental from './Dental';
-import Footer from './Footer';
+import Footer from '../Shared/Footer';
 import HomeContact from './HomeContact';
 import Info from './Info';
 import MakeAppointment from './MakeAppointment';
@@ -10,7 +10,7 @@ import Testimonials from './Testimonials';
 
 const Home = () => {
     return (
-        <div className='px-12'>
+        <div>
             <Banner></Banner>
             <Info></Info>
             <Services></Services>
