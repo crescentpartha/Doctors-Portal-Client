7e90791d88d1417bb7ae30d0e3b9b00c02cda031
crescentpartha <crescentpartha406@gmail.com>
2023-01-03 14:35:39 +0600

    Display a modal from on booking click with dynamic data

--- doctors-portal-client/src/Pages/Appointment/Service.js
+++ doctors-portal-client/src/Pages/Appointment/Service.js
@@ -1,19 +1,24 @@
 import React from 'react';
 
-const Service = ({ service }) => {
-    const {name, slots} = service;
+const Service = ({ service, setTreatment }) => {
+    const { name, slots } = service;
     return (
         <div className="card lg:max-w-lg bg-white shadow-xl">
             <div className="card-body text-center">
                 <h2 className="text-2xl text-primary">{name}</h2>
                 <p>{
                     slots.length > 0
-                    ? <span>{slots[0]}</span>
-                    : <span className='text-red-500'>Try another date</span>
-                    }</p>
+                        ? <span>{slots[0]}</span>
+                        : <span className='text-red-500'>Try another date</span>
+                }</p>
                 <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
                 <div className="card-actions justify-center">
-                    <button disabled={slots.length === 0} className="btn btn-primary text-white uppercase">Book Appointment</button>
+                    <label
+                        for="booking-modal"
+                        disabled={slots.length === 0}
+                        onClick={() => setTreatment(service)}
+                        className="btn btn-primary text-white uppercase"
+                    >Book Appointment</label>
                 </div>
             </div>
         </div>
