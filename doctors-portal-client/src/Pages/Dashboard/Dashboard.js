048e5e7e015cc81ac6e2b0e2c4ca93f425aeda09
crescentpartha <crescentpartha406@gmail.com>
2023-01-08 01:21:16 +0600

    Display User specific Appointment using Data Table

--- doctors-portal-client/src/Pages/Dashboard/Dashboard.js
+++ doctors-portal-client/src/Pages/Dashboard/Dashboard.js
@@ -4,20 +4,20 @@ import { Link, Outlet } from 'react-router-dom';
 const Dashboard = () => {
     return (
         <div className="drawer drawer-mobile">
-            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content flex flex-col items-center justify-center">
+            <input id="dashboard-sidebar" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content">
                 {/* <!-- Page content here --> */}
-                <h2 className='text-5xl text-purple-500'>Dashboard</h2>
+                <h2 className='text-2xl font-bold text-purple-500'>Welcome to your Dashboard</h2>
                 <Outlet></Outlet>
-                <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+                
 
             </div>
             <div className="drawer-side">
-                <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+                <label htmlFor="dashboard-sidebar" className="drawer-overlay"></label>
                 <ul className="menu p-4 w-80 bg-base-100 text-base-content">
                     {/* <!-- Sidebar content here --> */}
-                    <li><Link to='/dashboard'>Sidebar Item 1</Link></li>
-                    <li><Link to='/dashboard/review'>Sidebar Item 2</Link></li>
+                    <li><Link to='/dashboard'>My Appointment</Link></li>
+                    <li><Link to='/dashboard/review'>My Review</Link></li>
                 </ul>
 
             </div>
