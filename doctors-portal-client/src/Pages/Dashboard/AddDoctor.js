00cd91f3eecfc0138bbb9b7dff669e1bd0d0f1e0
crescentpartha <crescentpartha406@gmail.com>
2023-02-05 00:24:56 +0600

    Load services with data project and Add file type input

--- doctors-portal-client/src/Pages/Dashboard/AddDoctor.js
+++ doctors-portal-client/src/Pages/Dashboard/AddDoctor.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading';
 
 const AddDoctor = () => {
     const { register, formState: { errors }, handleSubmit } = useForm();
 
+    const { data: services, isLoading } = useQuery('services', () => fetch('https://doctors-portal-server-crescentpartha.vercel.app/service').then(res => res.json()));
+
     const onSubmit = async (data) => {
         console.log('data', data);
+    }
+
+    if (isLoading) {
+        return <Loading></Loading>
     }
 
     return (
@@ -67,19 +75,32 @@ const AddDoctor = () => {
                     <label className="label">
                         <span className="label-text">Specialty</span>
                     </label>
+                    <select {...register("specialty")} className="select w-full max-w-xs">
+                        {
+                            services.map(service => <option
+                                key={service._id}
+                                value={service.name}
+                            >{service.name}</option>)
+                        }
+                    </select>
+                </div>
+
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text">Photo</span>
+                    </label>
                     <input
-                        type="text"
-                        placeholder="specialty"
+                        type="file"
                         className="input input-bordered w-full max-w-xs"
-                        {...register("specialty", {
+                        {...register("image", {
                             required: {
                                 value: true,
-                                message: 'Specialization is Required'
+                                message: 'Image is Required'
                             }
                         })}
                     />
                     <label className="label">
-                        {errors.specialty?.type === 'required' && <span className="label-text-alt text-red-500">{errors.specialty?.message}</span>}
+                        {errors.image?.type === 'required' && <span className="label-text-alt text-red-500">{errors.image?.message}</span>}
                     </label>
                 </div>
 
