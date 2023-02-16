aede823c3cd0ffbc07ce79b8153ab3c328a3be8a
crescentpartha <crescentpartha406@gmail.com>
2023-02-17 02:11:16 +0600

    Basic Stipe setup with simple card form

--- doctors-portal-client/src/Pages/Dashboard/Payment.js
+++ doctors-portal-client/src/Pages/Dashboard/Payment.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import Loading from '../Shared/Loading';
+import CheckoutForm from './CheckoutForm';
+
+const stripePromise = loadStripe('pk_test_51McBOMDbKucfiCpSXbG2llpmMA8qgLyheqgTSevHc5ssASMgdgxxBCNKRm7Gx6YcLguv4Ch69X3IrliFN4d9T9c7007eJGqkKQ');
 
 const Payment = () => {
     const { id } = useParams();
@@ -30,7 +35,9 @@ const Payment = () => {
             </div>
             <div className="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
                 <div className="card-body">
-
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm></CheckoutForm>
+                    </Elements>
                 </div>
             </div>
         </div>
